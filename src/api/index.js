c2ec227c00290741c74e7d6c8026032bea9d544d
rheniere <davidlienardy1@gmail.com>
2017-07-29 14:36:14 +0700

    initial commit learn lerna

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by rheniere on 29/07/17.
+ */
+const express = require('express');
+const web = require('web');
+const app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    console.log(web(1, 5));
+    res.send('hello world');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
---
+++ src/web/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by rheniere on 29/07/17.
+ */
+module.exports = function test(a, b) {
+    return a + b;
+};
